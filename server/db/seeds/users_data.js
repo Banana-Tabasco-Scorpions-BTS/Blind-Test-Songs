106ef8af3d0015d1f305d37165ab50d9d71303b1
Rei <reidumand@outlook.com>
2022-11-02 11:58:17 +0900

    Fix: Troubleshoot Deploy seed errors 01

--- server/db/seeds/users_data.js
+++ server/db/seeds/users_data.js
@@ -4,10 +4,14 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('users').del()
-  await knex('users').insert([
-    {username: "Sanju"},
-    {username: "Rei"},
-    {username: "Cris"}
-  ]);
+  await knex.schema.hasTable('users').then(async function(exists) {
+    if (!exists) {
+      await knex('users').del()
+      await knex('users').insert([
+        {username: "Sanju"},
+        {username: "Rei"},
+        {username: "Cris"}
+      ]);
+    }
+  });
 };
