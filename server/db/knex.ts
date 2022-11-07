2f0c3b4db13c09ecb95e1c8d616e67a648cab8ce
haruna <haruna.kawakami867@gmail.com>
2022-11-07 16:46:14 +0900

    fix: add dotenv config

--- server/db/knex.ts
+++ server/db/knex.ts
@@ -1,6 +1,10 @@
-const config = require('../knexfile');
-import knex, { Knex } from 'knex';
+const config = require("../knexfile");
+require("dotenv").config();
+import knex, { Knex } from "knex";
 
-const db : Knex = process.env.NODE_ENV === 'production' ? knex(config.production) : knex(config.development);
+const db: Knex =
+  process.env.NODE_ENV === "production"
+    ? knex(config.production)
+    : knex(config.development);
 
 export default db;
